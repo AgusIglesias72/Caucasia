b989d104130ccfe2150e043d494105c9a641d5be
AgusIglesias72 <AgusIglesias72@gmail.com>
2021-09-08 18:15:19 -0300

    Cambie algunas cosas por funcionalidades de JQuery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 carrito = []
 
-const titulo = document.getElementById('title')
-titulo.classList.add('container', 'titulo')
+const titulo = $('#title')
+titulo.addClass('container', 'titulo')
 
-const contenedor = document.getElementById('contenedor-productos')
+const contenedor = $('#contenedor-productos')
 
-const tabla = document.getElementById('tabla-contenedor')
+const tabla = $('#tabla-contenedor')
 
 
 const mostrarCompra = () => {
@@ -30,21 +30,76 @@ const agregarCarrito = (prodId) => {
     mostrarCompra()
 }
 
-for (const producto of stockTotal) {
+const mostrarProductos = (array) => {
+    
+    contenedor.html = ""
 
-    const div = document.createElement('div')
+    for (const producto of array){
 
-    div.className = "card producto"
-    div.style.width = "18rem"
- 
-    div.innerHTML = `
-        <h3>${producto.nombre}</h3>
-        <img src=${producto.img}>
-        <p>$${producto.precio}</p>
-        <a onclick="agregarCarrito(${producto.id})" class="btn btn-primary" id="botonCarrito">Boton</a>
-    `    
-    contenedor.appendChild(div)
+        contenedor.append(`
+    
+            <div class="card producto">
+            <h3>${producto.nombre}</h3>
+            <img src=${producto.img}>
+            <p>$${producto.precio}</p>
+            <a onclick="agregarCarrito(${producto.id})" class="btn btn-primary" id="botonCarrito">Boton</a>
+            </div>
+            
+        `)
+    
+        $(".producto").css("width" , "22rem")
+    }
 }
+
+mostrarProductos(stockTotal)
+
+// FILTRADO
+const filtroTipo = $("#tipo")
+const filtroPrecio = $("#precios")
+
+const filtrado = () => {
+    let valorTipo = filtroTipo.value
+    let valorPrecio = filtroPrecio.value 
+    
+    let productosFiltrados = []
+
+    if (valorTipo == "Todos"){
+        productosFiltrados = stockTotal
+    } else {
+        productosFiltrados = stockTotal.filter( el => el.tipo == filtroTipo.value)
+    }
+
+    if (valorPrecio == 1){
+        productosFiltrados = productosFiltrados.filter( el => el.precio <= 1000)
+    } else if (valorPrecio == 2){
+        productosFiltrados = productosFiltrados.filter( el => el.precio >= 1000)
+    }
+
+    mostrarProductos(productosFiltrados)
+}
+
+filtroTipo.addEventListener('change', ()=>{
+    filtrado()
+})
+filtroPrecio.addEventListener('change', ()=>{
+    filtrado()
+})
+
+// for (const producto of stockTotal) {
+
+//     const div = document.createElement('div')
+
+//     div.className = "card producto"
+//     div.style.width = "18rem"
+ 
+//     div.innerHTML = `
+//         <h3>${producto.nombre}</h3>
+//         <img src=${producto.img}>
+//         <p>$${producto.precio}</p>
+//         <a onclick="agregarCarrito(${producto.id})" class="btn btn-primary" id="botonCarrito">Boton</a>
+//     `    
+//     contenedor.appendChild(div)
+// }
 
 
 console.log(carrito)
